8645cd0bd8b38a6ed2f5e702e2351ded402cbee5
Gabriel <gprando55@gmail.com>
2021-01-09 22:01:08 -0300

    feat: add cms :sparkles:

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd7fbc71011bafc332cd51eb18a720d5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '81172bf6-b725-4138-9b8a-ae20a4d0386d'
+};
